4fc28bbb685628c15f0d84ac133b905bd80d8c4d
Ameet Vadhia <ameet01@gmail.com>
2017-10-18 13:31:39 -0700

    Finished Associations Exercise

--- rails/app/models/course.rb
+++ rails/app/models/course.rb
@@ -3,4 +3,18 @@ class Course < ApplicationRecord
   class_name: :Enrollment,
   primary_key: :id,
   foreign_key: :course_id
+
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :user
+
+  belongs_to :prerequisite,
+  class_name: 'Course',
+  primary_key: :id,
+  foreign_key: :prereq_id
+
+  belongs_to :instructor,
+  class_name: :User,
+  primary_key: :id,
+  foreign_key: :instructor_id
 end
--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :enrolled_courses,
   through: :enrollments,
   source: :course
+
 end
